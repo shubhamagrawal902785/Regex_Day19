1b374ee5be9a018ab241a7a7bfc4ffd11f224a7d
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-11-30 23:38:54 +0530

    First Name validation

---
+++ src/com/bridgelabz/com/RegexValidation.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.com;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexValidation {
+
+	//Regex validation rules
+	
+	public static boolean checkValidName(String firstName) {
+        String firstNameRegex= "^[A-Z]{1}[A-Za-z]{2,}$";
+        Pattern obj= Pattern.compile(firstNameRegex);
+
+	if(firstName==null) {
+		return false;
+	
+	}
+	 Matcher obj2 = obj.matcher(firstName);
+     return obj2.matches();
+}
+
+	 public static void main(String[] args) {
+
+	        System.out.println("----- Welcome First To Name Validator -----");
+
+	        String firstName ="mango";
+	        boolean isfirstName = checkValidName(firstName);
+
+	        if(isfirstName)
+	            System.out.println(firstName+" is an Valid firstName");
+	        else
+	            System.out.println(firstName+" is an Invalid firstName");
+	    }
+}
+
+
+	
