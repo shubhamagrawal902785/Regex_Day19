751981e6270ff4d31d0af18875d89b4a5d5da289
shubhamagrawal902785 <114243771+shubhamagrawal902785@users.noreply.github.com>
2022-12-12 14:16:13 +0530

    Update RegexValidation.java

--- src/com/bridgelabz/com/RegexValidation.java
+++ src/com/bridgelabz/com/RegexValidation.java
@@ -1,36 +1,27 @@
 package com.bridgelabz.com;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.Scanner;
 public class RegexValidation {
 
 	//Regex validation rules
 	
-	public static boolean checkValidName(String firstName) {
-        String firstNameRegex= "^[A-Z]{1}[A-Za-z]{2,}$";
-        Pattern obj= Pattern.compile(firstNameRegex);
-
-	if(firstName==null) {
-		return false;
-	
-	}
-	 Matcher obj2 = obj.matcher(firstName);
-     return obj2.matches();
-}
-
 	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
 
-	        System.out.println("----- Welcome First To Name Validator -----");
+	        System.out.println("Enter the First name");
+	        String str1 = sc.next();
+	        
 
-	        String firstName ="Shubham";
-	        boolean isfirstName = checkValidName(firstName);
-
-	        if(isfirstName)
-	            System.out.println(firstName+" is an Valid firstName");
-	        else
-	            System.out.println(firstName+" is an Invalid firstName");
-	    }
+	        boolean b1 = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$").matcher(str1).matches();
+	       
+	        
+	        //Checking First name
+	        if (b1 == true){
+	            System.out.println("The first name is valid");
+	        }else
+	            System.err.println("The first name is Invalid");
 }
-
+}
 
 	
